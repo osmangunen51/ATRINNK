cc14985951f7ecb27ab9abc1d1588efff8db9600
tahsin-balci <tahsinbalci51@gmail.com>
2022-05-06 21:24:00 +0300

    Api GÜncellemeleri

--- MakinaTurkiye.Api/Controllers/MembershipController.cs
+++ MakinaTurkiye.Api/Controllers/MembershipController.cs
@@ -4,11 +4,13 @@ using MakinaTurkiye.Core.Infrastructure;
 using MakinaTurkiye.Entities.Tables.Logs;
 using MakinaTurkiye.Entities.Tables.Members;
 using MakinaTurkiye.Entities.Tables.Messages;
+using MakinaTurkiye.Entities.Tables.Stores;
 using MakinaTurkiye.Services.Authentication;
 using MakinaTurkiye.Services.Logs;
 using MakinaTurkiye.Services.Members;
 using MakinaTurkiye.Services.Messages;
 using MakinaTurkiye.Services.Stores;
+using MakinaTurkiye.Services.Packets;
 using System;
 using System.Configuration;
 using System.Globalization;
@@ -30,6 +32,7 @@ namespace MakinaTurkiye.Api.Controllers
         private readonly IMemberStoreService _memberStoreService;
         private readonly IAuthenticationService _authenticationService;
         private readonly IStoreService _storeService;
+        private readonly IPacketService _packetService;
 
         public MembershipController()
         {
@@ -40,25 +43,8 @@ namespace MakinaTurkiye.Api.Controllers
             _memberStoreService = EngineContext.Current.Resolve<IMemberStoreService>();
             _authenticationService = EngineContext.Current.Resolve<IAuthenticationService>();
             _storeService = EngineContext.Current.Resolve<IStoreService>();
+            _packetService = EngineContext.Current.Resolve<IPacketService>();
         }
-
-        //public MembershipController(IMemberService memberService,
-        //                         IMessagesMTService messagesMTService,
-        //                         IMessageService messageService,
-        //                         ILoginLogService loginLogService,
-        //                         IMemberStoreService memberStoreService,
-        //                         IAuthenticationService authenticationService,
-        //                         IStoreService storeService
-        //                         )
-        //{
-        //    this._memberService = memberService;
-        //    this._messagesMTService = messagesMTService;
-        //    this._messageService = messageService;
-        //    this._memberStoreService = memberStoreService;
-        //    this._loginLogService = loginLogService;
-        //    this._authenticationService = authenticationService;
-        //    this._storeService = storeService;
-        //}
 
         public HttpResponseMessage FastMembership([FromBody] UserRegister Model)
         {
@@ -528,284 +514,15 @@ namespace MakinaTurkiye.Api.Controllers
             {
                 var LoginUserEmail = Request.CheckLoginUserClaims().LoginMemberEmail;
 
-                var member = !string.IsNullOrEmpty(LoginUserEmail) ? _memberService.GetMemberByMemberEmail(LoginUserEmail) : null;
+                var loginmember = !string.IsNullOrEmpty(LoginUserEmail) ? _memberService.GetMemberByMemberEmail(LoginUserEmail) : null;
 
-                if (member != null)
+                if (loginmember != null)
                 {
-                    var memberMainParty = new MainParty
-                    {
-                        Active = false,
-                        MainPartyType = (byte)MainPartyType.Member,
-                        MainPartyRecordDate = DateTime.Now,
-                        MainPartyFullName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(member.MemberName.ToLower() + " " + member.MemberSurname.ToLower())
-                    };
-                    _memberService.InsertMainParty(memberMainParty);
-
-                    if (!string.IsNullOrEmpty(corporateInfoModelInput.birthDate))
-                    {
-                        var bithDate = new Convert.ToDateTime(corporateInfoModelInput.birthDate.ToString());
-                        member.BirthDate = bithDate;
-                    }
+                    processStatus.Message.Header = "CheckEmailForNewMember";
+                    processStatus.Message.Text = "İşlem başarılı";
+                    processStatus.Status = true;
+                    processStatus.Result = null;
                 }
-                
-                var model = SessionMembershipViewModel.MembershipViewModel;
-                if (string.IsNullOrWhiteSpace(model.MembershipModel.MemberName))
-                {
-                    Session["TimeOut"] = true;
-                    return RedirectToAction("HizliUyelik/UyelikTipi-20", "Uyelik");
-                }
-                
-
-
-               
-               
-
-                int memberMainPartyId = memberMainParty.MainPartyId;
-                var member = new Member
-                {
-                    MainPartyId = memberMainPartyId,
-                    BirthDate = model.MembershipModel.BirthDate,
-                    Gender = model.MembershipModel.Gender,
-                    MemberEmail = model.MembershipModel.MemberEmail,
-                    MemberName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(model.MembershipModel.MemberName.ToLower()),
-                    MemberPassword = model.MembershipModel.MemberPassword,
-                    Active = false,
-                    MemberType = (byte)MemberType.Enterprise,
-                    MemberSurname = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(model.MembershipModel.MemberSurname.ToLower()),
-                    MemberTitleType = model.MembershipModel.StoreAuthorizedTitleType,
-                    ActivationCode = activCode,
-                    ReceiveEmail = model.MembershipModel.ReceiveEmail
-                };
-                
-
-                string memberNo = "##";
-                for (int i = 0; i < 7 - memberMainPartyId.ToString().Length; i++)
-                {
-                    memberNo = memberNo + "0";
-                }
-                memberNo = memberNo + memberMainPartyId;
-                member.MemberNo = memberNo;
-
-                _memberService.InsertMember(member);
-
-                var storeMainParty = new MainParty
-                {
-                    Active = false,
-                    MainPartyType = (byte)MainPartyType.Firm,
-                    MainPartyRecordDate = DateTime.Now,
-                    MainPartyFullName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(model.MembershipModel.StoreName.ToLower()),
-                };
-                _memberService.InsertMainParty(storeMainParty);
-
-                var storeMainPartyId = storeMainParty.MainPartyId;
-
-
-                var packet = _packetService.GetPacketByIsStandart(true);
-                var store = new Store
-                {
-                    MainPartyId = storeMainPartyId,
-                    PacketId = packet.PacketId,
-                    StoreName = CultureInfo.CurrentCulture.TextInfo.ToTitleCase(model.MembershipModel.StoreName.ToLower()),
-                    StoreEMail = model.MembershipModel.MemberEmail,
-                    StoreWeb = model.MembershipModel.StoreWeb,
-                    StoreLogo = model.MembershipModel.StoreLogo,
-                    StoreActiveType = (byte)PacketStatu.Inceleniyor,
-                    StorePacketBeginDate = DateTime.Now,
-                    StorePacketEndDate = DateTime.Now.AddDays(packet.PacketDay),
-                    StoreAbout = model.MembershipModel.StoreAbout,
-                    StoreRecordDate = DateTime.Now,
-                    StoreEstablishmentDate = model.MembershipModel.StoreEstablishmentDate.HasValue ? model.MembershipModel.StoreEstablishmentDate.Value : 0,
-                    StoreCapital = model.MembershipModel.StoreCapital,
-                    StoreEmployeesCount = model.MembershipModel.StoreEmployeesCount,
-                    StoreEndorsement = model.MembershipModel.StoreEndorsement,
-                    StoreType = model.MembershipModel.StoreType,
-                    TaxOffice = model.MembershipModel.TaxOffice,
-                    TaxNumber = model.MembershipModel.TaxNumber,
-                    PurchasingDepartmentEmail = model.MembershipModel.PurchasingDepartmentEmail,
-                    PurchasingDepartmentName = model.MembershipModel.PurchasingDepartmentName,
-                    FounderText = string.Empty,
-                    GeneralText = string.Empty,
-                    HistoryText = string.Empty,
-                    PhilosophyText = string.Empty,
-                    ViewCount = 0,
-                    SingularViewCount = 0,
-                    StoreShowcase = false,
-                };
-
-                string storeNo = "###";
-                for (int i = 0; i < 6 - storeMainPartyId.ToString().Length; i++)
-                {
-                    storeNo = storeNo + "0";
-                }
-                storeNo = storeNo + storeMainPartyId;
-                store.StoreNo = storeNo;
-
-                _storeService.InsertStore(store);
-
-                var address = new Address
-                {
-                    MainPartyId = storeMainPartyId,
-                    Avenue = model.MembershipModel.Avenue,
-                    Street = model.MembershipModel.Street,
-                    DoorNo = model.MembershipModel.DoorNo,
-                    ApartmentNo = model.MembershipModel.ApartmentNo,
-                    AddressDefault = true,
-                };
-                if (model.MembershipModel.CountryId > 0)
-                    address.CountryId = model.MembershipModel.CountryId;
-                else
-                    address.CountryId = null;
-                if (model.MembershipModel.AddressTypeId > 0)
-                    address.AddressTypeId = model.MembershipModel.AddressTypeId;
-                else
-                    address.AddressTypeId = null;
-                if (model.MembershipModel.CityId > 0)
-                    address.CityId = model.MembershipModel.CityId;
-                else
-                    address.CityId = null;
-                if (model.MembershipModel.LocalityId > 0)
-                    address.LocalityId = model.MembershipModel.LocalityId;
-                else
-                    address.LocalityId = null;
-                if (model.MembershipModel.TownId > 0)
-                    address.TownId = model.MembershipModel.TownId;
-                else
-                    address.TownId = null;
-
-                _addressService.InsertAdress(address);
-
-                if (model.MembershipModel.InstitutionalPhoneNumber != null && !string.IsNullOrWhiteSpace(model.MembershipModel.InstitutionalPhoneNumber))
-                {
-                    var phone1 = new Phone
-                    {
-                        MainPartyId = storeMainPartyId,
-                        PhoneAreaCode = model.MembershipModel.InstitutionalPhoneAreaCode,
-                        PhoneCulture = model.MembershipModel.InstitutionalPhoneCulture,
-                        PhoneNumber = model.MembershipModel.InstitutionalPhoneNumber,
-                        PhoneType = (byte)PhoneType.Phone,
-                        GsmType = null
-                    };
-                    _phoneService.InsertPhone(phone1);
-                }
-                if (model.MembershipModel.InstitutionalPhoneNumber2 != null && !string.IsNullOrWhiteSpace(model.MembershipModel.InstitutionalPhoneNumber2))
-                {
-                    var phone2 = new Phone
-                    {
-                        MainPartyId = storeMainPartyId,
-                        PhoneAreaCode = model.MembershipModel.InstitutionalPhoneAreaCode2,
-                        PhoneCulture = model.MembershipModel.InstitutionalPhoneCulture2,
-                        PhoneNumber = model.MembershipModel.InstitutionalPhoneNumber2,
-                        PhoneType = (byte)PhoneType.Phone,
-                        GsmType = null
-                    };
-                    _phoneService.InsertPhone(phone2);
-                }
-                if (model.MembershipModel.InstitutionalGSMNumber != null && !string.IsNullOrWhiteSpace(model.MembershipModel.InstitutionalGSMNumber))
-                {
-                    var phoneGsm = new Phone
-                    {
-                        MainPartyId = storeMainPartyId,
-                        PhoneAreaCode = model.MembershipModel.InstitutionalGSMAreaCode,
-                        PhoneCulture = model.MembershipModel.InstitutionalGSMCulture,
-                        PhoneNumber = model.MembershipModel.InstitutionalGSMNumber,
-                        PhoneType = (byte)PhoneType.Gsm,
-                        GsmType = model.MembershipModel.GsmType
-                    };
-                    _phoneService.InsertPhone(phoneGsm);
-                }
-                if (model.MembershipModel.InstitutionalGSMNumber2 != null && !string.IsNullOrWhiteSpace(model.MembershipModel.InstitutionalGSMNumber2))
-                {
-                    var phoneGsm2 = new Phone
-                    {
-                        MainPartyId = storeMainPartyId,
-                        PhoneAreaCode = model.MembershipModel.InstitutionalGSMAreaCode2,
-                        PhoneCulture = model.MembershipModel.InstitutionalGSMCulture2,
-                        PhoneNumber = model.MembershipModel.InstitutionalGSMNumber2,
-                        PhoneType = (byte)PhoneType.Gsm,
-                        GsmType = model.MembershipModel.GsmType2
-                    };
-                    _phoneService.InsertPhone(phoneGsm2);
-                }
-                if (model.MembershipModel.InstitutionalFaxNumber != null && !string.IsNullOrWhiteSpace(model.MembershipModel.InstitutionalFaxNumber))
-                {
-                    var phoneFax = new Phone
-                    {
-                        MainPartyId = storeMainPartyId,
-                        PhoneAreaCode = model.MembershipModel.InstitutionalFaxAreaCode,
-                        PhoneCulture = model.MembershipModel.InstitutionalFaxCulture,
-                        PhoneNumber = model.MembershipModel.InstitutionalFaxNumber,
-                        PhoneType = (byte)PhoneType.Fax,
-                        GsmType = null
-                    };
-                    _phoneService.InsertPhone(phoneFax);
-                }
-                if (model.MembershipModel.ActivityName != null)
-                {
-                    for (int i = 0; i < model.MembershipModel.ActivityName.Length; i++)
-                    {
-                        if (model.MembershipModel.ActivityName.GetValue(i).ToString() != "false")
-                        {
-                            var storeActivityType = new StoreActivityType
-                            {
-                                StoreId = storeMainPartyId,
-                                ActivityTypeId = Convert.ToByte(model.MembershipModel.ActivityName.GetValue(i))
-                            };
-                            _storeActivityTypeService.InsertStoreActivityType(storeActivityType);
-                        }
-                    }
-                }
-                if (model.MembershipModel.StoreActivityCategory != null)
-                {
-                    var relCategory = model.MembershipModel.StoreActivityCategory.Where(c => c != "false").ToList();
-                    for (int i = 0; i < relCategory.Count(); i++)
-                    {
-                        var storeActivityCategory = new StoreActivityCategory
-                        {
-                            MainPartyId = storeMainPartyId,
-                            CategoryId = int.Parse(relCategory[i])
-                        };
-                        _storeActivityCategoryService.InsertStoreActivityCategory(storeActivityCategory);
-                    }
-                }
-                var memberStore = new MemberStore
-                {
-                    MemberMainPartyId = memberMainPartyId,
-                    StoreMainPartyId = storeMainPartyId,
-                    MemberStoreType = 1
-                };
-                _memberStoreService.InsertMemberStore(memberStore);
-
-                // firma  logo biçimendirme.
-                if (!string.IsNullOrEmpty(store.StoreName))
-                {
-                    string storeLogoFolder = this.Server.MapPath(AppSettings.StoreLogoFolder);
-                    string resizeStoreFolder = this.Server.MapPath(AppSettings.ResizeStoreLogoFolder);
-                    string storeLogoThumbSize = AppSettings.StoreLogoThumbSizes;
-                    List<string> thumbSizesForStoreLogo = new List<string>();
-                    thumbSizesForStoreLogo.AddRange(storeLogoThumbSize.Split(';'));
-                    var di = Directory.CreateDirectory(string.Format("{0}{1}", resizeStoreFolder, store.MainPartyId.ToString()));
-                    di.CreateSubdirectory("thumbs");
-                    string oldStoreLogoImageFilePath = string.Format("{0}{1}", storeLogoFolder, store.StoreLogo);
-                    if (System.IO.File.Exists(oldStoreLogoImageFilePath))
-                    {
-                        // eski logoyu kopyala, varsa ustune yaz
-                        string newStoreLogoImageFilePath = resizeStoreFolder + store.MainPartyId.ToString() + "\\";
-                        string newStoreLogoImageFileName = store.StoreName.ToImageFileName() + "_logo.jpg";
-                        System.IO.File.Copy(oldStoreLogoImageFilePath, newStoreLogoImageFilePath + newStoreLogoImageFileName, true);
-                        bool thumbResult = ImageProcessHelper.ImageResize(newStoreLogoImageFilePath + newStoreLogoImageFileName,
-                        newStoreLogoImageFilePath + "thumbs\\" + store.StoreName.ToImageFileName(), thumbSizesForStoreLogo);
-
-                        store.StoreLogo = newStoreLogoImageFileName;
-                        _storeService.UpdateStore(store);
-                    }
-                }
-                ActivationCodeSend(model.MembershipModel.MemberEmail, activCode, model.MembershipModel.MemberName + " " + model.MembershipModel.MemberSurname);
-
-
-                processStatus.Message.Header = "CheckEmailForNewMember";
-                processStatus.Message.Text = "İşlem başarılı";
-                processStatus.Status = true;
-                processStatus.Result = null;
             }
             catch (Exception ex)
             {
