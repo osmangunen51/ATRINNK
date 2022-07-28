b083ea07522fb8a3834c7c9bdc0634b8325b3889
Tuanna <Tuanna@DESKTOP-FGE6MNI>
2022-07-28 18:51:32 +0300

    Stabil Versiyon 1.

--- NeoSistem.MakinaTurkiye.Management/Content/Scripts/system.js
+++ NeoSistem.MakinaTurkiye.Management/Content/Scripts/system.js
@@ -1,4 +1,22 @@
 ﻿
+function bilgimesajver(title = "Bilgilendirme", message = "Lütfen Bekleyiniz...", position = "mid-center", icon ="info") {
+    $.toast({
+        heading: title,
+        text: message,
+        showHideTransition: 'slide',
+        textColor: '#eee',          
+        textAlign: 'left', 
+        position: position,
+        icon: icon,
+        loader: false,
+        loaderBg: '#9EC600'
+    });
+}
+
+function allMessageClose()
+{
+    $.toast().reset('all');
+}
 
 $(".date").datepicker({
     dateFormat: "dd.mm.yy",
@@ -8,65 +26,336 @@ $(".date").datepicker({
 });
 
 
-$(document).ready(function () {
 
-
-
-
-    var clipboard = new ClipboardJS('.btnkopyala');
-
-    $('.call').click(
-        function () {
-            var number = $(this).attr("data-number");
-            if (typeof (number) != 'undefined') {
-                data = {
-                    number: number
-                };
-                //$('#callcenterpanel-btn').trigger("click");
-                $.ajax({
-                    type: 'POST',
-                    url: '/CallCenter/Calling',
-                    data: data,
-                    dataType:'json',
-                    success: function (result) {
-                        if (result.IsSuccess)
-                        {
-                            //swal("Call Center", "Arama Çağrısı başlatıldı lüfen Arama Kontrol Ekranınızı Kontrol Ediniz.", "success");
+$(".dttable").each(function () {
+    var element = $(this);
+    var size = element.attr("data-size");
+    if (typeof (size) == "undefined") {
+        size = 25;
+    }
+    var dt = element.DataTable(
+        {
+            pageLength: size,
+            language: {
+                "emptyTable": "Tabloda herhangi bir veri mevcut değil",
+                "info": "_TOTAL_ kayıttan _START_ - _END_ arasındaki kayıtlar gösteriliyor",
+                "infoEmpty": "Kayıt yok",
+                "infoFiltered": "(_MAX_ kayıt içerisinden bulunan)",
+                "infoThousands": ".",
+                "lengthMenu": "Sayfada _MENU_ kayıt göster",
+                "loadingRecords": "Yükleniyor...",
+                "processing": "İşleniyor...",
+                "search": "Ara:",
+                "zeroRecords": "Eşleşen kayıt bulunamadı",
+                "paginate": {
+                    "first": "İlk",
+                    "last": "Son",
+                    "next": "Sonraki",
+                    "previous": "Önceki"
+                },
+                "aria": {
+                    "sortAscending": ": artan sütun sıralamasını aktifleştir",
+                    "sortDescending": ": azalan sütun sıralamasını aktifleştir"
+                },
+                "select": {
+                    "rows": {
+                        "_": "%d kayıt seçildi",
+                        "1": "1 kayıt seçildi"
+                    },
+                    "cells": {
+                        "1": "1 hücre seçildi",
+                        "_": "%d hücre seçildi"
+                    },
+                    "columns": {
+                        "1": "1 sütun seçildi",
+                        "_": "%d sütun seçildi"
+                    }
+                },
+                "autoFill": {
+                    "cancel": "İptal",
+                    "fillHorizontal": "Hücreleri yatay olarak doldur",
+                    "fillVertical": "Hücreleri dikey olarak doldur",
+                    "fill": "Bütün hücreleri <i>%d<\/i> ile doldur"
+                },
+                "buttons": {
+                    "collection": "Koleksiyon <span class=\"ui-button-icon-primary ui-icon ui-icon-triangle-1-s\"><\/span>",
+                    "colvis": "Sütun görünürlüğü",
+                    "colvisRestore": "Görünürlüğü eski haline getir",
+                    "copySuccess": {
+                        "1": "1 satır panoya kopyalandı",
+                        "_": "%ds satır panoya kopyalandı"
+                    },
+                    "copyTitle": "Panoya kopyala",
+                    "csv": "CSV",
+                    "excel": "Excel",
+                    "pageLength": {
+                        "-1": "Bütün satırları göster",
+                        "_": "%d satır göster"
+                    },
+                    "pdf": "PDF",
+                    "print": "Yazdır",
+                    "copy": "Kopyala",
+                    "copyKeys": "Tablodaki veriyi kopyalamak için CTRL veya u2318 + C tuşlarına basınız. İptal etmek için bu mesaja tıklayın veya escape tuşuna basın.",
+                    "createState": "Şuanki Görünümü Kaydet",
+                    "removeAllStates": "Tüm Görünümleri Sil",
+                    "removeState": "Aktif Görünümü Sil",
+                    "renameState": "Aktif Görünümün Adını Değiştir",
+                    "savedStates": "Kaydedilmiş Görünümler",
+                    "stateRestore": "Görünüm -&gt; %d",
+                    "updateState": "Aktif Görünümün Güncelle"
+                },
+                "searchBuilder": {
+                    "add": "Koşul Ekle",
+                    "button": {
+                        "0": "Arama Oluşturucu",
+                        "_": "Arama Oluşturucu (%d)"
+                    },
+                    "condition": "Koşul",
+                    "conditions": {
+                        "date": {
+                            "after": "Sonra",
+                            "before": "Önce",
+                            "between": "Arasında",
+                            "empty": "Boş",
+                            "equals": "Eşittir",
+                            "not": "Değildir",
+                            "notBetween": "Dışında",
+                            "notEmpty": "Dolu"
+                        },
+                        "number": {
+                            "between": "Arasında",
+                            "empty": "Boş",
+                            "equals": "Eşittir",
+                            "gt": "Büyüktür",
+                            "gte": "Büyük eşittir",
+                            "lt": "Küçüktür",
+                            "lte": "Küçük eşittir",
+                            "not": "Değildir",
+                            "notBetween": "Dışında",
+                            "notEmpty": "Dolu"
+                        },
+                        "string": {
+                            "contains": "İçerir",
+                            "empty": "Boş",
+                            "endsWith": "İle biter",
+                            "equals": "Eşittir",
+                            "not": "Değildir",
+                            "notEmpty": "Dolu",
+                            "startsWith": "İle başlar",
+                            "notContains": "İçermeyen",
+                            "notStarts": "Başlamayan",
+                            "notEnds": "Bitmeyen"
+                        },
+                        "array": {
+                            "contains": "İçerir",
+                            "empty": "Boş",
+                            "equals": "Eşittir",
+                            "not": "Değildir",
+                            "notEmpty": "Dolu",
+                            "without": "Hariç"
                         }
-                        else {
-                            swal("Call Center", result.Message, "warning");
-                        }
+                    },
+                    "data": "Veri",
+                    "deleteTitle": "Filtreleme kuralını silin",
+                    "leftTitle": "Kriteri dışarı çıkart",
+                    "logicAnd": "ve",
+                    "logicOr": "veya",
+                    "rightTitle": "Kriteri içeri al",
+                    "title": {
+                        "0": "Arama Oluşturucu",
+                        "_": "Arama Oluşturucu (%d)"
+                    },
+                    "value": "Değer",
+                    "clearAll": "Filtreleri Temizle"
+                },
+                "searchPanes": {
+                    "clearMessage": "Hepsini Temizle",
+                    "collapse": {
+                        "0": "Arama Bölmesi",
+                        "_": "Arama Bölmesi (%d)"
+                    },
+                    "count": "{total}",
+                    "countFiltered": "{shown}\/{total}",
+                    "emptyPanes": "Arama Bölmesi yok",
+                    "loadMessage": "Arama Bölmeleri yükleniyor ...",
+                    "title": "Etkin filtreler - %d",
+                    "showMessage": "Tümünü Göster",
+                    "collapseMessage": "Tümünü Gizle"
+                },
+                "thousands": ".",
+                "datetime": {
+                    "amPm": [
+                        "öö",
+                        "ös"
+                    ],
+                    "hours": "Saat",
+                    "minutes": "Dakika",
+                    "next": "Sonraki",
+                    "previous": "Önceki",
+                    "seconds": "Saniye",
+                    "unknown": "Bilinmeyen",
+                    "weekdays": {
+                        "6": "Paz",
+                        "5": "Cmt",
+                        "4": "Cum",
+                        "3": "Per",
+                        "2": "Çar",
+                        "1": "Sal",
+                        "0": "Pzt"
+                    },
+                    "months": {
+                        "9": "Ekim",
+                        "8": "Eylül",
+                        "7": "Ağustos",
+                        "6": "Temmuz",
+                        "5": "Haziran",
+                        "4": "Mayıs",
+                        "3": "Nisan",
+                        "2": "Mart",
+                        "11": "Aralık",
+                        "10": "Kasım",
+                        "1": "Şubat",
+                        "0": "Ocak"
                     }
-                });
-                event.preventDefault();
-            }
-            else {
-                swal("Call Center", "Numara Tanımsız", "warning")
-            }
-        }
-    ); 
-
-    $('#callcenterpanel-btn').click(
-        function () {
-            var callcenterurl = $(this).attr("data-callcenterurl");
-            if (typeof (callcenterurl) != 'undefined') {
-                var Wincheck = window.open(callcenterurl, "callcenterurl", "");
-                if (Wincheck.closed)
-                {
-
+                },
+                "decimal": ",",
+                "editor": {
+                    "close": "Kapat",
+                    "create": {
+                        "button": "Yeni",
+                        "submit": "Kaydet",
+                        "title": "Yeni kayıt oluştur"
+                    },
+                    "edit": {
+                        "button": "Düzenle",
+                        "submit": "Güncelle",
+                        "title": "Kaydı düzenle"
+                    },
+                    "error": {
+                        "system": "Bir sistem hatası oluştu (Ayrıntılı bilgi)"
+                    },
+                    "multi": {
+                        "info": "Seçili kayıtlar bu alanda farklı değerler içeriyor. Seçili kayıtların hepsinde bu alana aynı değeri atamak için buraya tıklayın; aksi halde her kayıt bu alanda kendi değerini koruyacak.",
+                        "noMulti": "Bu alan bir grup olarak değil ancak tekil olarak düzenlenebilir.",
+                        "restore": "Değişiklikleri geri al",
+                        "title": "Çoklu değer"
+                    },
+                    "remove": {
+                        "button": "Sil",
+                        "confirm": {
+                            "_": "%d adet kaydı silmek istediğinize emin misiniz?",
+                            "1": "Bu kaydı silmek istediğinizden emin misiniz?"
+                        },
+                        "submit": "Sil",
+                        "title": "Kayıtları sil"
+                    }
+                },
+                "stateRestore": {
+                    "creationModal": {
+                        "button": "Kaydet",
+                        "columns": {
+                            "search": "Kolon Araması",
+                            "visible": "Kolon Görünümü"
+                        },
+                        "name": "Görünüm İsmi",
+                        "order": "Sıralama",
+                        "paging": "Sayfalama",
+                        "scroller": "Kaydırma (Scrool)",
+                        "search": "Arama",
+                        "searchBuilder": "Arama Oluşturucu",
+                        "select": "Seçimler",
+                        "title": "Yeni Görünüm Oluştur",
+                        "toggleLabel": "Kaydedilecek Olanlar"
+                    },
+                    "duplicateError": "Bu Görünüm Daha Önce Tanımlanmış",
+                    "emptyError": "Görünüm Boş Olamaz",
+                    "emptyStates": "Herhangi Bir Görünüm Yok",
+                    "removeConfirm": "Görünümü Silmek İstediğinize Eminminisiniz?",
+                    "removeError": "Görünüm Silinemedi",
+                    "removeJoiner": "ve",
+                    "removeSubmit": "Sil",
+                    "removeTitle": "Görünüm Sil",
+                    "renameButton": "Değiştir",
+                    "renameLabel": "Görünüme Yeni İsim Ver -&gt; %s:",
+                    "renameTitle": "Görünüm İsmini Değiştir"
                 }
-                else
-                if (parseInt(navigator.appVersion > 2)) {
-                    if (Wincheck.closed)
-                    {
-
-                    }
-                    else
-                    {
-
-                    }
-                }
             }
-        }
-    );
+        });
 });
+
+
+var clipboard = new ClipboardJS('.btnkopyala');
+
+$('.call').click(
+    function () {
+        var number = $(this).attr("data-number");
+        if (typeof (number) != 'undefined') {
+            data = {
+                number: number
+            };
+            //$('#callcenterpanel-btn').trigger("click");
+            $.ajax({
+                type: 'POST',
+                url: '/CallCenter/Calling',
+                data: data,
+                dataType: 'json',
+                success: function (result) {
+                    if (result.IsSuccess) {
+                        $.toast({
+                            heading: 'Bilgilendirme',
+                            text: "Arama Çağrısı başlatıldı lüfen Arama Kontrol Ekranınızı Kontrol Ediniz.",
+                            showHideTransition: 'slide',  // It can be plain, fade or slide
+                            textColor: '#eee',            // text color
+                            textAlign: 'left',            // Alignment of text i.e. left, right, center
+                            position: 'top-right',
+                            icon: 'info',
+                            loader: true,        // Change it to false to disable loader
+                            loaderBg: '#9EC600'// bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+                        });
+
+                        //swal("Call Center", "Arama Çağrısı başlatıldı lüfen Arama Kontrol Ekranınızı Kontrol Ediniz.", "success");
+                    }
+                    else {
+                        swal("Call Center", result.Message, "warning");
+                    }
+                }
+            });
+            event.preventDefault();
+        }
+        else {
+            swal("Call Center", "Numara Tanımsız", "warning")
+        }
+    }
+);
+
+$('#callcenterpanel-btn').click(
+    function () {
+        $.toast({
+            heading: 'Bilgilendirme',
+            text: "Arama Çağrısı başlatıldı lüfen Arama Kontrol Ekranınızı Kontrol Ediniz.",
+            showHideTransition: 'slide',  // It can be plain, fade or slide
+            textColor: '#eee',            // text color
+            textAlign: 'left',            // Alignment of text i.e. left, right, center
+            position: 'top-right',
+            icon: 'info',
+            loader: true,        // Change it to false to disable loader
+            loaderBg: '#9EC600'// bottom-left or bottom-right or bottom-center or top-left or top-right or top-center or mid-center or an object representing the left, right, top, bottom values to position the toast on page
+        });
+        var callcenterurl = $(this).attr("data-callcenterurl");
+        if (typeof (callcenterurl) != 'undefined') {
+            var Wincheck = window.open(callcenterurl, "callcenterurl", "");
+            if (Wincheck.closed) {
+
+            }
+            else
+                if (parseInt(navigator.appVersion > 2)) {
+                    if (Wincheck.closed) {
+
+                    }
+                    else {
+
+                    }
+                }
+        }
+    }
+);
